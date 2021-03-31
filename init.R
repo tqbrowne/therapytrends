a667d428e1ddeebe9e389d83ef2129f2f1432469
tqbrowne <81526338+tqbrowne@users.noreply.github.com>
2021-03-31 09:01:11 -0400

    Update init.R

--- init.R
+++ init.R
@@ -14,4 +14,4 @@ install_if_missing = function(p) {
 
 invisible(sapply(my_packages, install_if_missing))
 
-install.packages("modeltime_0.50.tar.gz", repos=NULL, type="source")
+install.packages("/app/modeltime_0.5.0.tar.gz", repos=NULL, type="source")
