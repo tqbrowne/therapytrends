c107ccf562f987bcd264e36b71d1ccd95088ca52
tqbrowne <81526338+tqbrowne@users.noreply.github.com>
2021-03-31 10:01:52 -0400

    Update init.R

--- init.R
+++ init.R
@@ -14,5 +14,6 @@ install_if_missing = function(p) {
 
 invisible(sapply(my_packages, install_if_missing))
 
+install.packages("/app/rstan_2.21.2.tar.gz", repos=NULL, type="source")
 install.packages("/app/prophet_1.0.tar.gz", repos=NULL, type="source")
 install.packages("/app/modeltime_0.5.0.tar.gz", repos=NULL, type="source")
